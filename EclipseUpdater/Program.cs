2854ebca71e3e09ff2d43f87bb775da914379156
Zynato <z@mail.eclipseorigins.com>
2019-07-05 23:56:15 -0400

    Add stub api client for loading releases

--- EclipseUpdater/Program.cs
+++ EclipseUpdater/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Logging.Serilog;
+using EclipseUpdater.Api;
 
 namespace EclipseUpdater
 {
@@ -21,7 +24,20 @@ namespace EclipseUpdater
         // container, etc.
         private static void AppMain(Application app, string[] args)
         {
+            //Task.Run(TestReleases);
             app.Run(new MainWindow());
+        }
+
+        private static async Task TestReleases() {
+            var releasesClient = new ReleasesClient("https://releases.eclipseorigins.com");
+
+            var releases = await releasesClient.GetReleasesAsync(new Guid("0836bbd7-d9b4-466a-a566-7670bd568e3b"), 0, 1);
+
+            if (releases.Items.Count > 0) {
+                var latestRelease = releases.Items[0];
+
+                var downloadUrl = latestRelease.GetDownloadUrl();
+            }
         }
     }
 }
