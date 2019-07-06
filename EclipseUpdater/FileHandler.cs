616386da9c504132dc851829b13a52667f6bc255
MohenjoDaro <mohenjo.daro@hotmail.com>
2019-07-06 01:07:58 -0400

    - Added more functionality and removed redunant function

--- EclipseUpdater/FileHandler.cs
+++ EclipseUpdater/FileHandler.cs
@@ -7,59 +7,86 @@ namespace EclipseUpdater
 {
     class DirectoryHandler
     {
-        public static bool CheckDirectory(string pathTarget)
-        {
-            return Directory.Exists(pathTarget);
-        }
-
         public static bool CreateDirectory(string pathTarget)
         {
-            try
-            {
+            try {
                 Directory.CreateDirectory(pathTarget);
                 return true;
-            }
-            catch
-            {
+            } catch {
                 return false;
             }
         }
 
-        public static bool MoveDirectory(string pathTarget, string pathDestination)
+        public static bool MoveDirectory(string pathTarget, string pathDestination, bool onlyChildren = false)
         {
-            try
-            {
-                Directory.Move(pathTarget, pathDestination);
+            try {
+                if (onlyChildren)
+                {
+                    // Move all sub directories and files
+                    foreach (string dir in Directory.EnumerateDirectories(pathTarget))
+                    {
+                        Directory.Move(Path.Combine(pathTarget, dir), pathDestination);
+                    }
+                    foreach (string file in Directory.EnumerateFiles(pathTarget))
+                    {
+                        FileHandler.MoveFile(Path.Combine(pathTarget, file), pathDestination);
+                    }
+                }
+                else
+                {
+                    Directory.Move(pathTarget, pathDestination);
+                }
                 return true;
-            }
-            catch
-            {
+            } catch {
                 return false;
             }
         }
 
         public static bool RenameDirectory(string pathTarget, string nameDirectory)
         {
-            try
-            {
-                Directory.Move(pathTarget, Path.Combine(Directory.GetParent(pathTarget).FullName, nameDirectory));
+            try {
+                Directory.Move(pathTarget, nameDirectory);
                 return true;
+            } catch {
+                return false;
             }
-            catch
-            {
+        }
+
+        public static bool MarkForDeletionDirectory(string pathTarget)
+        {
+            try {
+                RenameDirectory(pathTarget, pathTarget + ".del");
+                return true;
+            } catch {
                 return false;
             }
         }
 
         public static bool DestroyDirectory(string pathTarget)
         {
-            try
-            {
+            try {
                 Directory.Delete(pathTarget);
                 return true;
+            } catch {
+                return false;
             }
-            catch
-            {
+        }
+
+        public static bool DestroyMarkedForDeletion(string pathTarget)
+        {
+            try {
+                foreach (string dir in Directory.EnumerateDirectories(pathTarget, "*.del"))
+                {
+                    DestroyDirectory(Path.Combine(pathTarget, dir));
+                }
+
+                foreach (string file in Directory.EnumerateFiles(pathTarget, "*.del"))
+                {
+                    FileHandler.DestroyFile(Path.Combine(pathTarget, file));
+                }
+
+                return true;
+            } catch {
                 return false;
             }
         }
@@ -67,11 +94,6 @@ namespace EclipseUpdater
 
     class FileHandler
     {
-        public static bool CheckFile(string pathTarget)
-        {
-            return File.Exists(pathTarget);
-        }
-
         public static bool ExtractFile(string pathTarget, string pathExtractTo)
         {
             return true;
@@ -79,39 +101,61 @@ namespace EclipseUpdater
 
         public static bool MoveFile(string pathTarget, string pathDestination)
         {
-            try
-            {
+            try {
                 File.Move(pathTarget, pathDestination);
                 return true;
-            }
-            catch
-            {
+            } catch {
                 return false;
             }
         }
 
         public static bool RenameFile(string pathTarget, string nameFile)
         {
-            try
-            {
+            try {
                 File.Move(pathTarget, Path.Combine(Path.GetDirectoryName(pathTarget), nameFile));
                 return true;
+            } catch {
+                return false;
             }
-            catch
-            {
+        }
+
+        public static bool MarkForDeletionFile(string pathTarget)
+        {
+            try {
+                RenameFile(pathTarget, pathTarget + ".del");
+                return true;
+            } catch {
                 return false;
             }
         }
 
         public static bool DestroyFile(string pathTarget)
         {
-            try
-            {
+            try {
                 File.Delete(pathTarget);
                 return true;
+            } catch {
+                return false;
             }
-            catch
-            {
+        }
+    }
+
+    class UpdateHandler
+    {
+        public static string[] CheckForUpdate(int idProject)
+        {
+            try {
+                return null; // array of needed updates
+            } catch {
+                return null;
+            }
+        }
+
+        public static bool DownloadUpdate(string pathTarget, int idProject, string idUpdate)
+        {
+            try {
+                return true;
+            } catch {
                 return false;
             }
         }
