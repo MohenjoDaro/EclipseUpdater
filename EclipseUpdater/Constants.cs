03930eea4dd4164eec4409bb043046fd8ca8ebc7
MohenjoDaro <mohenjo.daro@hotmail.com>
2019-08-10 16:25:32 -0400

    Added id, version, and date

--- EclipseUpdater/Constants.cs
+++ EclipseUpdater/Constants.cs
@@ -6,6 +6,8 @@ namespace EclipseUpdater
 {
     public static class Constants
     {
-        public static readonly Guid UpdaterId = Guid.Empty;
+        public static readonly Guid UpdaterId = new Guid("0742a489-2d38-46f9-affc-eeefe10dd645");
+        public static readonly string UpdaterVersion = "1.0.0";
+        public static readonly DateTime UpdaterDate = DateTime.Parse("2019-08-07T21:22:01.4185176Z");
     }
 }
